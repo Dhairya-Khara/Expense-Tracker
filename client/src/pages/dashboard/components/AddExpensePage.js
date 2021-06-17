cf3bbd2baebc80983fa5dd78b98dff03782b116c
Dhairya Khara <kharadhairya@gmail.com>
2021-06-16 21:32:25 -0400

    refactored, changed authentication redirect to constructor instead of render

--- client/src/pages/dashboard/components/AddExpensePage.js
+++ client/src/pages/dashboard/components/AddExpensePage.js
@@ -6,69 +6,63 @@ import Header from './Header'
 
 
 class AddExpensePage extends React.Component {
-    constructor(props){
+
+    constructor(props) {
         super(props)
+        //if not authenticated, redirect to landing page
+        if (props.auth === false) {
+            props.history.push("/")
+        }
     }
 
-    callAPI = ({description, createdAt, amount, note})=>{
+    //method that calls the api to add expense to database
+    callAPI = ({ description, createdAt, amount, note }) => {
         const id = uuidv4()
         let url = "http://localhost:8080/createExpense?description="
-        +encodeURIComponent(description)+"&createdAt="+encodeURIComponent(createdAt)+"&amount="
-        +encodeURIComponent(amount)+"&note="+encodeURIComponent(note)+"&email="+encodeURIComponent(this.props.email)+"&id="
-        +encodeURIComponent(id)
-        
-       
+            + encodeURIComponent(description) + "&createdAt=" + encodeURIComponent(createdAt) + "&amount="
+            + encodeURIComponent(amount) + "&note=" + encodeURIComponent(note) + "&email=" + encodeURIComponent(this.props.email) + "&id="
+            + encodeURIComponent(id)
+
+        //jwt token is sent using the header, is verified on the back end
         let h = new Headers({
             "Authorization": this.props.token
         })
+
         let req = new Request(url, {
             method: "POST",
             headers: h
         })
-        fetch(req).then(async(response, error)=>{
-            if(error){
-                console.log("error")
-                console.log(error)
-            }
-            console.log(await response)
+        fetch(req).then(async (response, error) => {
+
         })
     }
 
     render() {
-        if(this.props.auth === true){
-            return (
-    
-                <div>
-                    <Header/>
-                    <h1>Add Expense</h1>
-                    <ExpenseForm onSubmit={(expense) => {
-                        
-                  
-                        this.callAPI(expense)   
-                        setTimeout(()=>{
-                            this.props.history.push('/dashboard')
-                        }, 10)
-                        
-                    }} />
-                </div>
-            )
-        }
-        else if(this.props.auth === false ){
-            return(
-                <div>
-                    <p>Please Log in</p>
-                    {
-                        this.props.history.push("/")
-                    }
-                </div>
-            )
-        }
+
+        return (
+
+            <div>
+                <Header />
+                <h1>Add Expense</h1>
+                <ExpenseForm onSubmit={(expense) => {
+
+
+                    this.callAPI(expense)
+                    //time out is set because database needs to be updated BEFORE going to dashboard. Else, outdated dashboard is rendered even if database is updated
+                    setTimeout(() => {
+                        this.props.history.push('/dashboard')
+                    }, 10)
+
+                }} />
+            </div>
+        )
+
 
     }
 }
 
-const mapStateToProps= (state)=>{
-    return{
+const mapStateToProps = (state) => {
+    return {
         email: state.auth.email,
         token: state.auth.token,
         auth: state.auth.authenticated,
