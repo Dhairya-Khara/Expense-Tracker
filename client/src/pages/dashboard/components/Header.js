01dea02b24b5b0c1f221009a4bfcdb621a05b2e3
Dhairya Khara <kharadhairya@gmail.com>
2021-06-20 10:20:39 -0400

    refactored Header Component

--- client/src/pages/dashboard/components/Header.js
+++ client/src/pages/dashboard/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
+//Header component that is rendered on every page after user logs in
 const Header = () => {
     return (
         <header>
