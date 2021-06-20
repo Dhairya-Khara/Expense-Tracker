105bac4145bdc0ce0824eb7de6aaf95fc875d354
Dhairya Khara <kharadhairya@gmail.com>
2021-06-20 10:13:00 -0400

    refactored expense list

--- client/src/pages/dashboard/components/ExpenseList.js
+++ client/src/pages/dashboard/components/ExpenseList.js
@@ -3,11 +3,13 @@ import ExpenseListItem from './ExpenseListItem'
 import {connect} from 'react-redux'
 import selectedExpenses from '../selectors/expenses'
 
+//list of expenses user has
 const ExpenseList = (props)=>{
     
     return(
         <div>
            
+           {/* each expenses added in the list of an ExpenseListItem component */}
             {props.expenses.map((expense)=>{
                 
                 return <ExpenseListItem key = {expense.id}  props = {expense}/>
@@ -19,6 +21,7 @@ const ExpenseList = (props)=>{
 const mapStateToProps = (state)=>{
 
     return {
+        //only store selectedExpenses that satisfy the filters
         expenses: selectedExpenses(state.expenses, state.filters)
         
     }
