3111e874324321c0f91a50d67a1df1216930a5ba
Dhairya Khara <kharadhairya@gmail.com>
2021-06-20 10:16:40 -0400

    refactored ExpenseListFilters

--- client/src/pages/dashboard/components/ExpenseListFilters.js
+++ client/src/pages/dashboard/components/ExpenseListFilters.js
@@ -1,50 +1,60 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import {DateRangePicker} from 'react-dates'
-import {setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate} from '../actions/filters'
+import { connect } from 'react-redux'
+import { DateRangePicker } from 'react-dates'
+import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from '../actions/filters'
 
-class ExpenseListFilters extends React.Component{
+//component handling all the filters that can be applied to expenses
+class ExpenseListFilters extends React.Component {
     state = {
         calendarFocused: null
     }
-    onDatesChange = ({startDate, endDate})=>{
+    //method that handles the calendar widget
+    onDatesChange = ({ startDate, endDate }) => {
         this.props.dispatch(setStartDate(startDate))
         this.props.dispatch(setEndDate(endDate))
     }
-    onFocusChange = (calendarFocused)=>{
-        this.setState(()=>{
-            return{
+    //method that handles if user is selected on calender widget or not
+    onFocusChange = (calendarFocused) => {
+        this.setState(() => {
+            return {
                 calendarFocused
             }
         })
     }
-    render(){
+    render() {
         return (
             <div>
-                <input type="text" value = {this.props.filters.text} onChange = {(e)=>{
+                {/* input dealing with text filter */}
+                <input type="text" value={this.props.filters.text} onChange={(e) => {
                     this.props.dispatch(setTextFilter(e.target.value))
-     
-                }}></input>
-                <select onChange = {(e)=>{
-                    if(e.target.value === "date"){
+
+                }}>
+
+                </input>
+
+                {/* input dealing with date filter */}
+                <select onChange={(e) => {
+                    if (e.target.value === "date") {
                         this.props.dispatch(sortByDate())
                     }
-                    else if(e.target.value === "amount"){
+                    else if (e.target.value === "amount") {
                         this.props.dispatch(sortByAmount())
                     }
                 }}>
-                    <option value = "date">Date</option>
-                    <option value = "amount">Amount</option>
+                {/* user may choose to filter between DATE or AMOUNT */}
+                    <option value="date">Date</option>
+                    <option value="amount">Amount</option>
                 </select>
+                {/* values required to use calendar widget */}
                 <DateRangePicker
-                    startDate = {this.props.filters.startDate}
-                    endDate = {this.props.filters.endDate}
-                    onDatesChange = {this.onDatesChange}
-                    focusedInput = {this.state.calendarFocused}
-                    onFocusChange = {this.onFocusChange}
-                    showClearDates = {true}
-                    numberOfMonths = {1}
-                    isOutsideRange = {()=>false}
+                    startDate={this.props.filters.startDate}
+                    endDate={this.props.filters.endDate}
+                    onDatesChange={this.onDatesChange}
+                    focusedInput={this.state.calendarFocused}
+                    onFocusChange={this.onFocusChange}
+                    showClearDates={true}
+                    numberOfMonths={1}
+                    isOutsideRange={() => false}
                 />
             </div>
         )
@@ -52,8 +62,8 @@ class ExpenseListFilters extends React.Component{
 }
 
 
-const mapStateToProps = (state)=>{
-    return({
+const mapStateToProps = (state) => {
+    return ({
         filters: state.filters
     })
 }
