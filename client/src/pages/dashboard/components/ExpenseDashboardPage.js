3ac7195f7d22339ad9529c5e238f82fb0860242a
Dhairya Khara <kharadhairya@gmail.com>
2021-06-20 09:58:02 -0400

    refactored and changed redirect to be in constructor rather than render in ExpenseDashbordPage

--- client/src/pages/dashboard/components/ExpenseDashboardPage.js
+++ client/src/pages/dashboard/components/ExpenseDashboardPage.js
@@ -5,7 +5,7 @@ import ExpenseList from './ExpenseList'
 import ExpenseListFilters from './ExpenseListFilters'
 import { addExpense, resetExpenseReducer } from '../actions/expenses'
 import { changeAuth } from '../../../actions/auth'
-import {resetFilterReducer} from '../actions/filters'
+import { resetFilterReducer } from '../actions/filters'
 import { Link } from 'react-router-dom'
 import Header from './Header'
 
@@ -15,15 +15,18 @@ import Header from './Header'
 class ExpenseListDashboard extends React.Component {
     constructor(props) {
         super(props)
-
+        if (props.auth === false) {
+            props.history.push("/")
+        }
     }
 
 
     componentWillMount() {
 
-
+        //clearing reduce expenser before filling it up from the database to avoid duplicate data
         this.props.dispatch(resetExpenseReducer())
 
+        //calling api to get all the expenses
         const url = "http://localhost:8080/getExpenses?email=" + encodeURIComponent(this.props.email)
         let h = new Headers({
             "Authorization": "Bearer " + this.props.token
@@ -35,12 +38,12 @@ class ExpenseListDashboard extends React.Component {
 
         fetch(req).then((response) => {
             response.json().then((data) => {
-        
+                //filling the expenses in the reducer one by one
                 for (let i = 0; i < data.length; i++) {
-                   
+
                     this.props.dispatch(addExpense({ description: data[i].description, note: data[i].note, amount: parseInt(data[i].amount), createdAt: parseInt(data[i].createdAt), id: data[i].id }))
 
-              
+
                 }
 
             })
@@ -48,6 +51,7 @@ class ExpenseListDashboard extends React.Component {
 
     }
 
+    //method called when logout button is clicked
     logout = (e) => {
 
 
@@ -67,11 +71,13 @@ class ExpenseListDashboard extends React.Component {
             console.log(error)
 
         })
+
+        //changing auth to false, and resetting expense and filter reducers
         this.props.dispatch(changeAuth(false, undefined, undefined))
         this.props.dispatch(resetExpenseReducer())
         this.props.dispatch(resetFilterReducer())
-        // this.props.history.push('/')
-      
+
+
 
     }
 
@@ -79,32 +85,23 @@ class ExpenseListDashboard extends React.Component {
 
     render() {
 
-        if (this.props.auth === true) {
-            return (
-                <div>
-                    <Header></Header>
-                    <h3>Expense List for {this.props.email}</h3>
-                  
-                    <Link to = "/">
-                        <button onClick={this.logout}>Logout</button>
-                   
-                    </Link>
 
-                    <ExpenseListFilters />
-                    <ExpenseList />
-                </div>
-            )
-        }
-        else if (this.props.auth === false) {
-            return (
-                <div>
-                 
-                    {
-                        this.props.history.push("/")
-                    }
-                </div>
-            )
-        }
+        return (
+            <div>
+                <Header></Header>
+                <h3>Expense List for {this.props.email}</h3>
+
+                <Link to="/">
+                    <button onClick={this.logout}>Logout</button>
+
+                </Link>
+
+                <ExpenseListFilters />
+                <ExpenseList />
+            </div>
+        )
+
+
 
     }
 
